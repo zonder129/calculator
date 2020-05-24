57694245fac5ae2701e354bb3317f5cf29aa94ac
zonder129 <havofavo@mail.ru>
2020-05-24 23:00:56 +0300

    add calculator logic based on reverse polish notation add test cases for exceptional / boundary cases

--- src/main/kotlin/KotlinCalculator.kt
+++ src/main/kotlin/KotlinCalculator.kt
@@ -1,6 +1,9 @@
 package org.jetbrains
 
+import java.math.BigDecimal
+import java.util.*
 import kotlin.IllegalStateException
+import kotlin.NoSuchElementException
 
 /*
 Create a simple calculator that given a string of operators (), +, -, *, / and numbers separated by spaces
@@ -8,18 +11,129 @@ returns the value of that expression
 
 Example: evaluate("1 + 1") => 2
 */
-fun evaluate(str: String): Double {
-    val values = str.split(" ")
-    if (values.size < 3) {
-        throw IllegalStateException("Less then 3 operations or operand in expression")
+class KotlinCalculator {
+    fun evaluate(str: String): Double {
+        val expression = str.split(" ").filter { value -> value.isNotBlank() }
+        val rpnExpression = transformIntoRPN(expression)
+        return calculateInRpn(rpnExpression)
     }
-    return when(values[1]){
-        "*" -> values[0].toDouble() * values[2].toDouble()
-        "/" -> values[0].toDouble() / values[2].toDouble()
-        "-" -> values[0].toDouble() - values[2].toDouble()
-        "+" -> values[0].toDouble() + values[2].toDouble()
-        else -> throw IllegalStateException("Operation is not expected: ${values[1]}")
+
+    private fun calculateInRpn(rpnExpression: Deque<String>): Double {
+        val tempValues = mutableListOf<String>()
+        while (rpnExpression.size != 1 || !isNumber(rpnExpression.first())) {
+            val value = rpnExpression.pop()
+            if (isOperation(value)) {
+                when (tempValues.size) {
+                    2 -> {
+                        val calculatedValue = applyOperation(tempValues[0], tempValues[1], value)
+                        rpnExpression.push(calculatedValue)
+                    }
+                    1 -> {
+                        if (value != "-" && value != "+") throw IllegalStateException("Only + and - allowed as unary operation")
+                        val calculatedValue = applyOperation(
+                            secondOperand = tempValues[0],
+                            operation = value
+                        )
+                        rpnExpression.push(calculatedValue)
+                    }
+                    else -> {
+                        throw IllegalStateException("More than 2 numbers provided in expression: $tempValues")
+                    }
+                }
+                tempValues.clear()
+            } else {
+                tempValues.add(value)
+            }
+        }
+        return rpnExpression.first().toDouble()
+    }
+
+    private fun applyOperation(firstOperand: String = "0", secondOperand: String, operation: String): String {
+        return when (operation) {
+            "+" -> {
+                firstOperand.toDouble() + secondOperand.toDouble()
+            }
+            "-" -> {
+                firstOperand.toDouble() - secondOperand.toDouble()
+            }
+            "*" -> {
+                firstOperand.toDouble() * secondOperand.toDouble()
+            }
+            "/" -> {
+                firstOperand.toDouble() / secondOperand.toDouble()
+            }
+            else -> throw IllegalStateException("Unexpected operation: $operation")
+        }.toString()
+    }
+
+    /**
+     * transform expression from infix notation into reverse polish notation
+     */
+    private fun transformIntoRPN(expression: List<String>): Deque<String> {
+        val operationsStack = ArrayDeque<String>()
+        val reversePolishNotation = ArrayDeque<String>()
+
+        expression.forEachIndexed { index, value ->
+            // TODO: unary minus
+            when {
+                value == ")" -> {
+                    var lastOperation: String? = null
+                    while (lastOperation != "(") {
+                        lastOperation?.let {
+                            reversePolishNotation.add(it)
+                        }
+                        if (operationsStack.isEmpty()) {
+                            throw IllegalStateException("Found undisclosed closing bracket in expression at position $index")
+                        }
+                        lastOperation = operationsStack.pop()
+                    }
+                }
+                value == "(" -> {
+                    operationsStack.push(value)
+                }
+                isOperation(value) -> {
+                    if (operationsStack.isNotEmpty()) {
+                        val operationPriority = getOperationPriority(value)
+                        val stackOpPriority = getOperationPriority(operationsStack.first)
+                        while (operationsStack.isNotEmpty() && operationPriority <= stackOpPriority) {
+                            reversePolishNotation.add(operationsStack.pop())
+                        }
+                    }
+                    operationsStack.push(value)
+                }
+                isNumber(value) -> {
+                    reversePolishNotation.add(value)
+                }
+                else -> {
+                    throw IllegalStateException("Unexpected token (neither number or operation) $value at position $index")
+                }
+            }
+        }
+
+        if (operationsStack.contains("(")) {
+            throw IllegalStateException("Found undisclosed opening bracket in expression")
+        }
+
+        while (operationsStack.isNotEmpty()) {
+            reversePolishNotation.add(operationsStack.pop())
+        }
+
+        return reversePolishNotation
+    }
+
+    private fun isNumber(value: String): Boolean = value.toDoubleOrNull() != null
+
+    private fun isOperation(value: String): Boolean = value in listOf("+", "-", "*", "/", "(")
+
+    private fun getOperationPriority(operation: String): Int {
+        return when (operation) {
+            "*", "/" -> 3
+            "+", "-" -> 2
+            "(" -> 1
+            else -> throw IllegalStateException("Unexpected operation: $operation")
+        }
     }
 }
 
-// ( 1 + 2 ) * 3
+
+
--- src/test/kotlin/org/jetbrains/KotlinCalculatorTest.kt
+++ src/test/kotlin/org/jetbrains/KotlinCalculatorTest.kt
@@ -1,17 +1,149 @@
 package org.jetbrains
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 
 class KotlinCalculatorTest {
+
+    val calcDelta = 1e-4
+
     @ParameterizedTest
     @CsvFileSource(resources = ["/operations_pairwise.csv"], numLinesToSkip = 1)
-    @DisplayName("All binary operations should work as expected")
-    fun operationCalculation(firstOperand: String, operator: String, secondOperand: String, expected: String) {
+    @DisplayName("All binary operations should be calculated properly")
+    fun operationCalculation(
+        firstOperand: String,
+        operator: String,
+        secondOperand: String,
+        expected: String
+    ) {
         val expression = "$firstOperand $operator $secondOperand"
-        assertEquals( expected.toDouble(), evaluate(expression), 1e-4)
+        val actual = KotlinCalculator().evaluate(expression)
+        assertEquals(expected.toDouble(), actual, calcDelta)
+    }
+
+    @Nested
+    inner class ExceptionalCases {
+        @Test
+        @DisplayName("Very large positive numbers (more than Double) results into infinity")
+        fun biggestPositiveNumber() {
+            val expression = "${Double.MAX_VALUE} + ${Double.MAX_VALUE}"
+            val actual = KotlinCalculator().evaluate(expression)
+            assertEquals(Double.POSITIVE_INFINITY, actual, "Positive infinity expected")
+        }
+
+        @Test
+        @DisplayName("Very large negative numbers (more than Double) results into -infinity")
+        fun biggestNegativeNumber() {
+            val expression = "-${Double.MAX_VALUE} - ${Double.MAX_VALUE} "
+            val actual = KotlinCalculator().evaluate(expression)
+            assertEquals(Double.NEGATIVE_INFINITY, actual, "Negative infinity expected")
+        }
+
+        @Test
+        @DisplayName("Very small positive fraction results into zero")
+        fun minimumFractionNumber() {
+            val expression = "${Double.MIN_VALUE} * 0.1"
+            val actual = KotlinCalculator().evaluate(expression)
+            assertEquals(0.0, actual, "Zero expected")
+        }
+
+        @Test
+        @DisplayName("Not a numbers can't be processed")
+        fun notANumber() {
+            val expression = "foo + bar"
+            val ex = assertThrows(IllegalStateException::class.java) {
+                KotlinCalculator().evaluate(expression)
+            }
+            LOG.info("Exception caught: ${ex.message}")
+        }
+
+        @Test
+        @DisplayName("Not an operation can't be processed")
+        fun notAnOperation() {
+            val expression = "10 versus 5"
+            val ex = assertThrows(IllegalStateException::class.java) {
+                KotlinCalculator().evaluate(expression)
+            }
+            LOG.info("Exception caught: ${ex.message}")
+        }
+
+        @Test
+        @DisplayName("Comma as fraction symbol can't be processed")
+        fun fractionComma() {
+            val expression = "10,5 - 5,3"
+            val ex = assertThrows(IllegalStateException::class.java) {
+                KotlinCalculator().evaluate(expression)
+            }
+            LOG.info("Exception caught: ${ex.message}")
+        }
+
+        @Test
+        @DisplayName("Double fraction dot symbol can't be processed")
+        fun doubleFractionDot() {
+            val expression = "10..5 - 5.3"
+            val ex = assertThrows(IllegalStateException::class.java) {
+                KotlinCalculator().evaluate(expression)
+            }
+            LOG.info("Exception caught: ${ex.message}")
+        }
+
+        @Test
+        @DisplayName("Common fractions can't be processed")
+        fun commonFraction() {
+            val expression = "3/4 - 1/2"
+            val ex = assertThrows(IllegalStateException::class.java) {
+                KotlinCalculator().evaluate(expression)
+            }
+            LOG.info("Exception caught: ${ex.message}")
+        }
+
+        @Test
+        @DisplayName("Numbers with leading zeros processed as usual numbers")
+        fun leadingZeros() {
+            val expression = "5 + 0003"
+            val actual = KotlinCalculator().evaluate(expression)
+            assertEquals(8.0, actual, calcDelta,"Numbers are not the same")
+        }
+
+        @Test
+        @DisplayName("Expression with missing open bracket can't be processed")
+        fun missingOpenBracket() {
+            val expression = "7 + 10 )"
+            val ex = assertThrows(IllegalStateException::class.java) {
+                KotlinCalculator().evaluate(expression)
+            }
+            LOG.info("Exception caught: ${ex.message}")
+        }
+
+        @Test
+        @DisplayName("Expression with missing close bracket can't be processed")
+        fun missingCloseBracket() {
+            val expression = "( 7 + 10"
+            val ex = assertThrows(IllegalStateException::class.java) {
+                KotlinCalculator().evaluate(expression)
+            }
+            LOG.info("Exception caught: ${ex.message}")
+        }
+
+        @Test
+        @DisplayName("Expression with extra spaces calculated properly")
+        fun extraSpaces() {
+            val expression = "15  + 12 "
+            val actual = KotlinCalculator().evaluate(expression)
+            assertEquals(27.0, actual, calcDelta,"Numbers are not the same")
+        }
+
+    }
+
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(KotlinCalculatorTest::class.java)
     }
 }
